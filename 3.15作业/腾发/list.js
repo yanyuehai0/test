f78656b366733da79168c6a7e8e58c282791be6f
admin <yanyuehai123@163.com>
2023-03-15 19:36:32 +0800

    3.15作业

---
+++ 3.15作业/腾发/list.js
@@ -0,0 +1,225 @@
+//https://www.tf2sc.cn/c0
+
+var list = [
+    {
+      ind: "202107151725080963",
+      carName: "2019款 揽胜星脉(进口) 250PS",
+      dateOfRegistration: 1546272000000,
+      currentPrice: 34.9,
+      mileage: 3.16,
+      saleType: 0,
+      cover:
+        "https://tfcar.oss-cn-hangzhou.aliyuncs.com/315192015060992000.jpg?x-oss-process=style/watermark",
+      reportAssess: true,
+    },
+    {
+      ind: "202106160917050115",
+      carName: "2013款 兰德酷路泽(进口) 4000 中东版",
+      dateOfRegistration: 1420041600000,
+      currentPrice: 42.9,
+      mileage: 11.87,
+      saleType: 0,
+      cover:
+        "https://tfcar.oss-cn-hangzhou.aliyuncs.com/315436741664178176.jpg?x-oss-process=style/watermark",
+      reportAssess: true,
+    },
+    {
+      ind: "202106151508020106",
+      carName: "2014款 保时捷Cayenne(进口) 3.6T 手自一体 美规版",
+      dateOfRegistration: 1435680000000,
+      currentPrice: 30.9,
+      mileage: 15.54,
+      saleType: 0,
+      cover:
+        "https://tfcar.oss-cn-hangzhou.aliyuncs.com/315437538758098944.jpg?x-oss-process=style/watermark",
+      reportAssess: true,
+    },
+    {
+      ind: "202106161151190127",
+      carName: "2007款 捷豹XJ(进口) XJ6L 新一代皇家加长版",
+      dateOfRegistration: 1196438400000,
+      currentPrice: 26.9,
+      mileage: 16.68,
+      saleType: 0,
+      cover:
+        "https://tfcar.oss-cn-hangzhou.aliyuncs.com/278907000484528128.jpg?x-oss-process=style/watermark",
+      reportAssess: true,
+    },
+    {
+      ind: "202111041104077324",
+      carName: "2011款 辉腾(进口) 3.6L V6 5座加长商务版",
+      dateOfRegistration: 1320076800000,
+      currentPrice: 8.5,
+      mileage: 19.4,
+      saleType: 2,
+      cover:
+        "https://tfcar.oss-cn-hangzhou.aliyuncs.com/315446937446776832.jpg?x-oss-process=style/watermark",
+      reportAssess: false,
+    },
+    {
+      ind: "202105281658410081",
+      carName: "2018款 雷克萨斯ES(进口) 300h 尊享版 国V",
+      dateOfRegistration: 1546272000000,
+      currentPrice: 29.9,
+      mileage: 4.28,
+      saleType: 0,
+      cover:
+        "https://tfcar.oss-cn-hangzhou.aliyuncs.com/315438592526647296.jpg?x-oss-process=style/watermark",
+      reportAssess: true,
+    },
+    {
+      ind: "202106181849230656",
+      carName: "2018款 奥迪Q3 30周年型 30 TFSI 时尚型",
+      dateOfRegistration: 1546272000000,
+      currentPrice: 13.9,
+      mileage: 2.74,
+      saleType: 0,
+      cover:
+        "https://tfcar.oss-cn-hangzhou.aliyuncs.com/315192953750421504.jpg?x-oss-process=style/watermark",
+      reportAssess: true,
+    },
+    {
+      ind: "202111160952167692",
+      carName: "2007款 奥迪Q7(进口) 4.2 FSI quattro 舒适型",
+      dateOfRegistration: 1206979200000,
+      currentPrice: 8.8,
+      mileage: 21.38,
+      saleType: 2,
+      cover:
+        "https://tfcar.oss-cn-hangzhou.aliyuncs.com/197650309642715136.jpeg?x-oss-process=style/watermark",
+      reportAssess: false,
+    },
+    {
+      ind: "202112040957088478",
+      carName: "2012款 奔驰S级(进口) S400L HYBRID Grand Edition",
+      dateOfRegistration: 1356969600000,
+      currentPrice: 23.9,
+      mileage: 14.44,
+      saleType: 0,
+      cover:
+        "https://tfcar.oss-cn-hangzhou.aliyuncs.com/315525820443525120.jpg?x-oss-process=style/watermark",
+      reportAssess: false,
+    },
+    {
+      ind: "202206231620296751",
+      carName: "2018款 普拉多 3.5L 自动 SX",
+      dateOfRegistration: 1543593600000,
+      currentPrice: 39.9,
+      mileage: 2.94,
+      saleType: 0,
+      cover:
+        "https://tfcar.oss-cn-hangzhou.aliyuncs.com/277490719768182784.jpeg?x-oss-process=style/watermark",
+      reportAssess: false,
+    },
+    {
+      ind: "202202211545563351",
+      carName: "2017款 奥迪A7(进口) 40 TFSI 进取型",
+      dateOfRegistration: 1517414400000,
+      currentPrice: 34.9,
+      mileage: 3.53,
+      saleType: 0,
+      cover:
+        "https://tfcar.oss-cn-hangzhou.aliyuncs.com/299191766844702720.jpg?x-oss-process=style/watermark",
+      reportAssess: false,
+    },
+    {
+      ind: "202203151603393740",
+      carName: "2018款 奥迪A7(进口) 35 TFSI 时尚型",
+      dateOfRegistration: 1533052800000,
+      currentPrice: 31.9,
+      mileage: 6.6,
+      saleType: 0,
+      cover:
+        "https://tfcar.oss-cn-hangzhou.aliyuncs.com/315519840460734464.jpg?x-oss-process=style/watermark",
+      reportAssess: false,
+    },
+    {
+      ind: "202109141143405151",
+      carName: "2017款 Stelvio(进口) 2.0T 200HP 豪华版",
+      dateOfRegistration: 1559318400000,
+      currentPrice: 24.98,
+      mileage: 3.1,
+      saleType: 3,
+      cover:
+        "https://tfcar.oss-cn-hangzhou.aliyuncs.com/174847867490402304.png?x-oss-process=style/watermark",
+      reportAssess: false,
+    },
+    {
+      ind: "202106161722390206",
+      carName: "2016款 领航员(进口) 3.5T AWD",
+      dateOfRegistration: 1485878400000,
+      currentPrice: 32.9,
+      mileage: 10.77,
+      saleType: 0,
+      cover:
+        "https://tfcar.oss-cn-hangzhou.aliyuncs.com/315431130058719232.jpg?x-oss-process=style/watermark",
+      reportAssess: true,
+    },
+    {
+      ind: "202111251345088223",
+      carName: "2016款 保时捷Panamera Edition 3.0T",
+      dateOfRegistration: 1459440000000,
+      currentPrice: 53.9,
+      mileage: 6.09,
+      saleType: 0,
+      cover:
+        "https://tfcar.oss-cn-hangzhou.aliyuncs.com/297713498127859712.jpg?x-oss-process=style/watermark",
+      reportAssess: false,
+    },
+    {
+      ind: "202109100954524341",
+      carName: "2014款 奔驰A级AMG(进口) A45 4MATIC",
+      dateOfRegistration: 1422720000000,
+      currentPrice: 18.9,
+      mileage: 5.32,
+      saleType: 0,
+      cover:
+        "https://tfcar.oss-cn-hangzhou.aliyuncs.com/299609241407193088.jpeg?x-oss-process=style/watermark",
+      reportAssess: false,
+    },
+    {
+      ind: "202108160932141641",
+      carName: "2018款 别克GL8 ES 28T 旗舰型 国VI",
+      dateOfRegistration: 1514736000000,
+      currentPrice: 23.9,
+      mileage: 5.2,
+      saleType: 1,
+      cover:
+        "https://tfcar.oss-cn-hangzhou.aliyuncs.com/317640272676651008.jpg?x-oss-process=style/watermark",
+      reportAssess: true,
+    },
+    {
+      ind: "202206091827496386",
+      carName: "2018款 威霆 2.0T 精英版 9座 国VI",
+      dateOfRegistration: 1593532800000,
+      currentPrice: 25.9,
+      mileage: 2.89,
+      saleType: 0,
+      cover:
+        "https://tfcar.oss-cn-hangzhou.aliyuncs.com/272069614228209664.jpeg?x-oss-process=style/watermark",
+      reportAssess: false,
+    },
+    {
+      ind: "202201020947359354",
+      carName: "2017款 奔驰S级(进口) S400L",
+      dateOfRegistration: 1483200000000,
+      currentPrice: 51.9,
+      mileage: 12.5,
+      saleType: 0,
+      cover:
+        "https://tfcar.oss-cn-hangzhou.aliyuncs.com/315500273265016832.jpg?x-oss-process=style/watermark",
+      reportAssess: false,
+    },
+    {
+      ind: "202106161732210213",
+      carName: "2017款 凯路威(进口) 2.0TSI 四驱舒适版 7座",
+      dateOfRegistration: 1514736000000,
+      currentPrice: 29.9,
+      mileage: 3.51,
+      saleType: 0,
+      cover:
+        "https://tfcar.oss-cn-hangzhou.aliyuncs.com/315429453289226240.jpg?x-oss-process=style/watermark",
+      reportAssess: true,
+    },
+  ];
+  
