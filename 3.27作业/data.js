de9bb79512cabdf3a50feaf97ac847826626e110
admin <yanyuehai123@163.com>
2023-04-01 14:19:42 +0800

    3.23到3.31作业

---
+++ 3.27作业/data.js
@@ -0,0 +1,98 @@
+const data = [
+    //  src 地址前面拼接  https://www.jq22.com/demo/tab202012021404  自身去掉./
+    // 例如 https://www.jq22.com/demo/tab202012021404/assets/images/t1.png
+  
+    {
+      type: "热血",
+      title: "火影忍者",
+      alt: "火影忍者",
+      src: "https://gd-hbimg.huaban.com/6c331b37669313648ce24854497f2a60cdfb1d125beb5-7q5zYe_fw240webp",
+    },
+    {
+      type: "热血",
+      title: "七龙珠",
+      alt: "七龙珠",
+      src: "https://gd-hbimg.huaban.com/c911cad7173d286c7f897923c300d7765978304417467e-J5yEWq_fw240webp",
+    },
+    {
+      type: "热血",
+      title: "海贼王",
+      alt: "海贼王",
+      src: "https://gd-hbimg.huaban.com/2ed85eabae9a1164a6e80583d4cf87c4283ef583143ee-0wlw4c_fw240webp",
+    },
+    {
+      type: "热血",
+      title: "妖精的尾巴",
+      alt: "妖精的尾巴",
+      src: "https://gd-hbimg.huaban.com/006c60d6e0c0ccbc33f45a8e47725fe779b1ca6d27584-TowR9h_fw240webp",
+    },
+    {
+      type: "热血",
+      title: "死神",
+      alt: "死神",
+      src: "https://gd-hbimg.huaban.com/f61bb4588650d77e38818ab633e0451a8e944fbc176090-POS9kW_fw240webp",
+    },
+    {
+      type: "冒险",
+      title: "海贼王",
+      alt: "海贼王",
+      src: "https://gd-hbimg.huaban.com/fa1ce3505b51643dccc6e80d239a4557b23abd6917fc21-ffAyZK_fw240webp",
+    },
+    {
+      type: "冒险",
+      title: "星游记",
+      alt: "星游记",
+      src: "https://gd-hbimg.huaban.com/53e5ed9512523e8610dc42bead0f9258153edfebf2064-EkwVfp_fw240webp",
+    },
+    {
+      type: "冒险",
+      title: "Fate/stay night",
+      alt: "Fate/stay night",
+      src: "https://gd-hbimg.huaban.com/dd1f51f11ed7eee36cef20b8915e00169e1ad5a1c2bdd-prY8so_fw240webp",
+    },
+    {
+      type: "冒险",
+      title: "数码宝贝",
+      alt: "数码宝贝",
+      src: "https://gd-hbimg.huaban.com/d873885a78b2b2d8e132184ea10f3719d9c6540d31ad6-pT1roI_fw240webp",
+    },
+    {
+      type: "冒险",
+      title: "光能使者",
+      alt: "光能使者",
+      src: "https://gd-hbimg.huaban.com/3331ee8442facf5b5d7a4f284ca98018262bfdfc173e84-1SGftP_fw240webp",
+    },
+    {
+      type: "冒险",
+      title: "火影忍者",
+      alt: "火影忍者",
+      src: "https://gd-hbimg.huaban.com/bedaa99b9ed456ea0cbbd864946a49c52a8e43f1ad679-xq9OCE_fw240webp",
+    },
+    {
+      type: "冒险",
+      title: "七龙珠",
+      alt: "七龙珠",
+      src: "https://gd-hbimg.huaban.com/b4624900f18430b9c41d4533590d7c5185e0773133474-9n16NT_fw240webp",
+    },
+    {
+      type: "竞技",
+      title: "游戏王",
+      alt: "游戏王",
+      src: "https://gd-hbimg.huaban.com/69a1da04850fbc70fde475049b38e870b84d60cb3615d1-vpavQA_fw240webp",
+    },
+    {
+      type: "竞技",
+      title: "灌篮高手",
+      alt: "灌篮高手",
+      src: "https://gd-hbimg.huaban.com/1d00d91829bf74618a594591e2e95fa30eb16bc9cff56-Ew0XWS_fw240webp",
+    },
+    {
+      type: "竞技",
+      title: "四驱兄弟",
+      alt: "四驱兄弟",
+      src: "https://gd-hbimg.huaban.com/04bfd1c70642e634b31977c9ff653334c694a4f21724fe-5GNa6A_fw240webp",
+    },
+  ];
+  
+  // https://www.jq22.com/demo/tab202012021404/assets/images/t1.png
+  
